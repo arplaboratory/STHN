20da59aa4487764b83ee9d2fdff3241ff80dde10
Jiuhong Xiao <jx1190@log-1.hpc.nyu.edu>
2024-02-02 12:57:44 -0500

    val threshold = sparse database

--- global_pipeline/eval.py
+++ global_pipeline/eval.py
@@ -60,9 +60,9 @@ args.save_dir = join(
 commons.setup_logging(args.save_dir)
 commons.make_deterministic(args.seed)
 if args.use_sparse_database!= -1:
-    logging.info(f"Using sparse sampling database. Reset the train and val positive threshold to {np.ceil(np.sqrt(2*(args.use_sparse_database)**2))}")
-    args.val_positive_dist_threshold = np.ceil(np.sqrt(2*(args.use_sparse_database)**2))
-    args.train_positives_dist_threshold = np.ceil(np.sqrt(2*(args.use_sparse_database)**2))
+    logging.info(f"Using sparse sampling database. Reset the train and val positive threshold to {args.use_sparse_database}")
+    args.val_positive_dist_threshold = args.use_sparse_database
+    args.train_positives_dist_threshold = args.use_sparse_database
 logging.info(f"Arguments: {args}")
 logging.info(f"The outputs are being saved in {args.save_dir}")
 
