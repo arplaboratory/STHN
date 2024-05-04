7aa5eb28787fcbc590029a622d17236ff0179d2d
Jiuhong Xiao <jx1190@log-1.hpc.nyu.edu>
2024-05-04 11:41:20 -0400

    fix name

--- train_local.sh
+++ train_local.sh
@@ -27,8 +27,8 @@
 # sbatch scripts/local_larger/train_local_sparse_256_extended_long.sbatch
 # sbatch scripts/local_larger/train_local_sparse_512_extended_long.sbatch
 
-## LARGER Permute
-# sbatch scripts/local_larger_permute/train_local_sparse_512_extended_long.sbatch
+## LARGER augment
+# sbatch scripts/local_larger_augment/train_local_sparse_512_extended_long.sbatch
 
 ## 2 stage
 # sbatch scripts/local_larger_2/train_local_dense_extended_load_f_aug64_c.sbatch
@@ -37,5 +37,5 @@
 # sbatch scripts/local_larger_2/train_local_sparse_256_extended_long_load_f_aug64_c.sbatch
 # sbatch scripts/local_larger_2/train_local_sparse_512_extended_long_load_f_aug64_c.sbatch
 
-## 2 stage 
-# sbatch scripts/local_larger_permute/train_local_sparse_512_extended_long_load_f_aug64_c.sbatch
+## 2 stage augment
+# sbatch scripts/local_larger_augment/train_local_sparse_512_extended_long_load_f_aug64_c.sbatch
